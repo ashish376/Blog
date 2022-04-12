b24c0b6f7628536c75beab6c7121bbaa70581780
ashish376 <84447975+ashish376@users.noreply.github.com>
2022-04-12 12:08:24 +0530

    Add files via upload

---
+++ PhpBlog/home.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+  </head>
+  <style type="text/css">
+    #inputbtn:hover{cursor:pointer;}
+  </style>
+  <body style="background:url('bulb.jpg'); background-size: 500px; ">
+    <div class="container-fluid" style="margin-top:60px;margin-bottom:60px;color:#34495E;">
+      <div class="row">
+        <div class="col-md-1"></div>
+        <div class="col-md-4">
+          <div class="card">
+            <img src="coffee.jpg" class="card-img-top">
+            <div class="card-body">
+              <center>
+              <h1 style=" 
+  text-shadow: 1px 1px 1px red,
+               2px 2px 1px red;
+">Admin Login</h5><br>
+              <p style=" font-family: Helvetica, Arial, sans-serif;"><b>Sometimes I think of blogging as finger exercises for a violinist; sometimes I think of it as mulching a garden. It is incredibly useful and helpful to my “real” writing.</b></p>
+              <form class="form-group" method="POST" action="login.php"style="background-color:orange;">
+                
+              <center><input type="submit" id="inputbtn" name="login_submit" value="Login" class="btn btn-primary"></center></marquee> 
+              </form>
+            
+              
+            </center>
+            </div>
+          </div>
+        </div>
+         <div class="col-md-7"></div>
+      </div>
+    </div>
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+  </body>
+</html>
